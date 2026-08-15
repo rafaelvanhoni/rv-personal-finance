e6a6cec6fe9c05de06393950b4a7a41fb3f8cb12
Rafael Vanhoni <rafael.vanhoni@gmail.com>
2026-08-15 19:11:43 -0300

    refactor: remove duplicated register/login flow from ProtectedEndpointTests

--- tests/RvPersonalFinance.Tests/Integration/ProtectedEndpointTests.cs
+++ tests/RvPersonalFinance.Tests/Integration/ProtectedEndpointTests.cs
@@ -1,17 +1,14 @@
 using System.Net;
-using System.Net.Http.Json;
 using System.Net.Http.Headers;
-using RvPersonalFinance.Api.Features.Auth;
 
 namespace RvPersonalFinance.Tests.Integration;
 
-public class ProtectedEndpointTests : IClassFixture<CustomWebApplicationFactory>
+public class ProtectedEndpointTests : IntegrationTestBase
 {
-    private readonly HttpClient _client;
 
-    public ProtectedEndpointTests(CustomWebApplicationFactory factory)
+    public ProtectedEndpointTests(CustomWebApplicationFactory factory) : base(factory)
     {
-        _client = factory.CreateClient();
+
     }
 
     [Fact]
@@ -30,27 +27,8 @@ public class ProtectedEndpointTests : IClassFixture<CustomWebApplicationFactory>
     public async Task ProtectedEndpoint_WithValidToken_ShouldReturnSuccess()
     {
         // Given
-        var requestRegister = new RegisterDto()
-        {
-            Name = "Junior",
-            Email = "junior@teste.com",
-            Password = "123467"
-        };
-        var requestLogin = new LoginDto()
-        {
-            Email = "junior@teste.com",
-            Password = "123467"
-        };
-
-        var responseRegister = await _client.PostAsJsonAsync("/auth/register", requestRegister);
-        Assert.Equal(HttpStatusCode.Created, responseRegister.StatusCode);
-
-        var responseLogin = await _client.PostAsJsonAsync("/auth/login", requestLogin);
-        var loginResult = await responseLogin.Content.ReadFromJsonAsync<ResponseEnvelope<LoginResponseDto>>();
-        Assert.NotNull(loginResult);
-        Assert.NotNull(loginResult.Data);
-
-        var token = loginResult.Data.Token;
+        var user = await RegisterAndLoginAsync();
+        var token = user.Login.Token;
 
         _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
